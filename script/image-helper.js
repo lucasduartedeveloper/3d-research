f64e8abb4624ab7e002b19b02f1c90067dc8b414
* <lucasduartedeveloper@gmail.com>
2022-10-27 20:20:42 -0300

    first commit

--- script/image-helper.js
+++ script/image-helper.js
@@ -42,18 +42,18 @@ var cropSquare = function(url, callback) {
     offsetX = 0;
     offsetY = 0;
 
-    if (width < height) {
-       if (width > max_size) {
-           height *= max_size / width;
-           width = max_size;
-       }
-       offsetY = ((height-max_size)/2)*-1;
-   } else {
+    if (width > height) {
        if (height > max_size) {
            width *= max_size / height;
            height = max_size;
        }
-       offsetX = ((width-max_size)/2)*-1;;
+       //offsetX = ((width-max_size)/2)*-1;
+   } else {
+       if (width > max_size) {
+           height *= max_size / width;
+           width = max_size;
+       }
+       //offsetY = ((height-max_size)/2)*-1;
    }
    canvas.width = width;
    canvas.height = height;
