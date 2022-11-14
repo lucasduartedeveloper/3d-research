4a0d52590038d26422e9c86db9a0c1c9c83e453a
* <lucasduartedeveloper@gmail.com>
2022-11-14 02:55:04 -0300

    first commit

--- 2D3D/script/main.js
+++ 2D3D/script/main.js
@@ -50,6 +50,7 @@ var drawItems = function() {
              item.style.top = k*slotWidth+"px";
              item.style.width = slotWidth+"px";
              item.style.height = slotWidth+"px";
+             item-style.shadow = "inset 2px 2px rgba(0,0,0,0.5)";
              box.appendChild(item);
         }
     }
