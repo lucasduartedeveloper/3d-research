26b6e258af4956d59bffad67002d24087d2aaa15
* <lucasduartedeveloper@gmail.com>
2022-11-14 20:01:21 -0300

    first commit

---
+++ 2D3D/ajax/database.php
@@ -0,0 +1,53 @@
+<?php include ('config/db.php')?>
+<?php
+//header("Content-Type: application/json; charset=utf-8");
+$sql ="";
+try {
+    $remote_addr = $_SERVER["REMOTE_ADDR"];
+    $forwarded =  $_SERVER["HTTP_X_FORWARDED_FOR"];
+
+    if (!empty($_POST["action"]) && 
+        $_POST["action"] == "update-account") {
+
+        $value = htmlspecialchars($_POST["value"]);
+        $sql = "UPDATE param SET value='".$value."' WHERE id=1";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+
+        echo $sql;
+    }
+    else if (!empty($_POST["action"]) && 
+        $_POST["action"] == "get-account") {
+
+        $sql = "SELECT value FROM param WHERE id=1";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $rowCount = $stmt->rowCount();
+        $details = $stmt->fetchAll(); 
+
+        echo $sql;
+    }
+    else if (!empty($_POST["action"]) && 
+        $_POST["action"] == "get-config") {
+
+        $sql = "SELECT value FROM param WHERE id=0";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $rowCount = $stmt->rowCount();
+        $details = $stmt->fetchAll(); 
+
+        echo $sql;
+    }
+}
+catch (PDOException $e) {
+   echo 'Connection failed: ' . $e->getMessage();
+   echo $sql;
+}
+catch (Exception $e) {
+    echo 'Error: ' . $e->getMessage();
+    echo $sql;
+}
+?>
